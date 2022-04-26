c02904da3eebd99ccd6086c0d88db8b029d7afd8
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-04-26 21:56:35 +0500

    add use repository

---
+++ src/Application/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace Application;
+
+public interface IUserRepository
+{
+    public Task<List<User>> GetByIdAsync(int id);
+    public Task<List<User>> GetAllAsync();
+}
---
+++ src/Infrastructure/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application;
+using Domain;
+
+namespace Infrastructure;
+
+public class UserRepository : IUserRepository
+{
+    public Task<List<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<User>> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
