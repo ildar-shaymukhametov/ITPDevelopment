beb9731ef28f552e245603dcba9da9a64b7f78ad
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-04-27 01:13:03 +0500

    wip

--- src/Client/Form1.cs
+++ src/Client/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Application;
+using Domain;
 using Microsoft.Extensions.Logging;
 
 namespace Client
@@ -22,6 +23,44 @@ namespace Client
             this.logger = logger;
             this.userRepository = userRepository;
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await PopulateTreeViewAsync();
+        }
+
+        private async Task PopulateTreeViewAsync()
+        {
+            TreeNode rootNode;
+            var users = await userRepository.GetAllAsync();
+
+            DirectoryInfo info = new DirectoryInfo(@"../..");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(users, rootNode);
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(List<User> users, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            List<User> subSubDirs;
+            foreach (var subDir in users)
+            {
+                aNode = new TreeNode(subDir.FirstName, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.Users.ToList();
+                if (subSubDirs.Any())
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
         }
     }
 }
