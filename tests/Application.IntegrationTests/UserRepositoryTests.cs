abb95b45428beddfcc0544f1962d8f8fdacb6089
Шаймухаметов Ильдар <ildar.shaymukhametov@gmail.com>
2022-04-27 10:26:10 +0500

    refactor tests

--- tests/Application.IntegrationTests/UserRepositoryTests.cs
+++ tests/Application.IntegrationTests/UserRepositoryTests.cs
@@ -11,13 +11,11 @@ public class UserRepositoryTests
     [Fact]
     public async Task GetById__Finds_user_by_id()
     {
-        var firstName = "Foo";
-        User user;
+        var user = new User { FirstName = "Foo" };
 
         using var factory = new ApplicationDbContextFactory();
         using (var context = factory.CreateContext())
         {
-            user = new User { FirstName = firstName };
             context.Users.Add(user);
             await context.SaveChangesAsync();
         }
@@ -27,7 +25,7 @@ public class UserRepositoryTests
             var sut = new UserRepository(context);
             var actual = await sut.GetByIdAsync(user.Id);
 
-            Assert.Equal(firstName, actual?.FirstName);
+            Assert.Equal(user.FirstName, actual?.FirstName);
         }
     }
 
@@ -35,18 +33,17 @@ public class UserRepositoryTests
     public async Task GetById__Loads_users_tree()
     {
         var firstName = "Foo";
-        User user;
+        var user = new User
+        {
+            Users = new List<User>
+            {
+                new() { FirstName = firstName }
+            }
+        };
 
         using var factory = new ApplicationDbContextFactory();
         using (var context = factory.CreateContext())
         {
-            user = new User
-            {
-                Users = new List<User>
-                {
-                    new User { FirstName = firstName }
-                }
-            };
             context.Users.Add(user);
             await context.SaveChangesAsync();
         }
@@ -56,23 +53,19 @@ public class UserRepositoryTests
             var sut = new UserRepository(context);
             var actual = await sut.GetByIdAsync(user.Id);
 
-            Assert.Collection(user.Users, x => Assert.Equal(firstName, x.FirstName));
+            Assert.Collection(actual.Users, x => Assert.Equal(firstName, x.FirstName));
         }
     }
 
     [Fact]
     public async Task GetAll__Returns_all_users()
     {
-        var firstNameA = "Foo";
-        var firstNameB = "Bar";
-        User userA;
-        User userB;
+        var userA = new User { FirstName = "Foo" };
+        var userB = new User { FirstName = "Bar" };
 
         using var factory = new ApplicationDbContextFactory();
         using (var context = factory.CreateContext())
         {
-            userA = new User { FirstName = firstNameA };
-            userB = new User { FirstName = firstNameB };
             context.Users.Add(userA);
             context.Users.Add(userB);
             await context.SaveChangesAsync();
@@ -84,8 +77,8 @@ public class UserRepositoryTests
             var actual = await sut.GetAllAsync();
 
             Assert.Collection(actual,
-                x => Assert.Equal(firstNameA, x.FirstName),
-                x => Assert.Equal(firstNameB, x.FirstName));
+                x => Assert.Equal(userA.FirstName, x.FirstName),
+                x => Assert.Equal(userB.FirstName, x.FirstName));
         }
     }
 }
